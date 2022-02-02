0f82740876a1ccac97cf643c3bede1b17cff7a42
Mike Stmria <coderstitch@gmail.com>
2022-02-02 15:44:51 -0600

    Created routes for index and notes per READme Acceptance Criteria

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// REQUIRE & CREATE ROUTER
+const index = require('express').Router();
+// REQUIRE HELPERS
+const { readAppend, readFromFile} = require('../helpers/fsHelpers');
+
+
+// GET ROUTE RESPONSE
+index.get('/', (req, res) => {
+    res.sendFIle('../public/index.html');
+});
--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,10 @@
-const notesRouter = require('express').Router();
+// REQUIRE & CREATE ROUTER
+const notes = require('express').Router();
+// REQUIRE HELPERS
+const { readAppend, readFromFile} = require('../helpers/fsHelpers');
+
+
+// GET ROUTE RESPONSE
+notes.get('/notes', (req, res) => {
+    res.sendFile('../public/notes.html')
+});
