bd2b442157bca2bcb2e65d684136d6cd33c9e82d
Mike Stmria <coderstitch@gmail.com>
2022-02-04 06:40:51 -0600

    Created server.js file

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,37 @@
 // REQUIRE & CREATE ROUTER
 const api = require('express').Router();
 // REQUIRE HELPERS
+const { v4: uuidv4 } = require('uuid');
 const { readAppend, readFromFile} = require('../helpers/fsHelpers');
 // REQUIRE DB
 const db = require('../db/db.json');
+const notes = require('./notes');
 
 
 // GET ROUTE RESPONSE
-api.get('/api', (req, res) => {
+api.get('/api/notes', (req, res) => {
     res.json(db);
 });
 
 // POST API ROUTE
-api.post('/api', (req, res) => {
+api.post('/api/notes', (req, res) => {
 
-})
+    const {note, tip} = req.body;
+
+    if (req.body) {
+        
+    const newNote = {
+        note,
+        tip,
+        tip_id: uuidv4()
+    };
+
+    readAppend(newNote, '../db/db.json');
+    res.json('Note added successfully');
+    } else {
+        res.error('Error adding note.')
+    };
+});
+
+
+module.export = api;
