2bfc5a30f7efcc6b46084d3b062e8e56c4e9c3a0
StitchCodes <coderstitch@gmail.com>
2022-04-21 20:42:20 -0500

    Correct server.js file

--- routes/api.js
+++ routes/api.js
@@ -5,31 +5,6 @@ const { v4: uuidv4 } = require('uuid');
 const { readAppend, readFromFile} = require('../helpers/fsHelpers');
 // REQUIRE DB
 const db = require('../db/notesDB.json');
-const notes = require('./notes');
-
-
-// GET ROUTE RESPONSE
-api.get('/', (req, res) => {
-    res.json(db);
-});
-
-// POST API ROUTE
-api.post('/', (req, res) => {
-    const {  title, text } = req.body;
-
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            tip_id: uuidv4(),
-        };
-
-        readAppend(newNote, './db/notesDB.json');
-        res.json(`Note added successfully 🚀`);
-    } else {
-        res.error('Error in adding note');
-    }
-});
 
 
 module.export = api;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
-const indexRouter = require('./routes/index.js');
 const apiRouter = require('./routes/index.js');
-const notesRouter = require('./routes/notes.js');
 
 // Create PORT
 const PORT = process.env.PORT || 3001;
@@ -10,27 +8,19 @@ const PORT = process.env.PORT || 3001;
 // Create app
 const app = express();
 
-
-// Parser
+// Middleware for parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-// app.use('/api', apiRouter);
-// app.use('/index', indexRouter);
-// app.use('/notes', notesRouter);
-
 app.use(express.static('public'));
 
-// ROUTES
 // Notes Route
 app.get('/notes', (req, res) => 
 res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-// Index/Wildcard route
+// Wildcard route redirects to index.html
 app.get('*', (req,res) => 
 res.sendFile(path.join(__dirname, '/public/index.html')));
-
-
 
 // App Listen PORT
 app.listen(PORT, () =>
